b47984f27dd19d5026908a2c74bd8bcb41b4eddd
Paulo-Eli <115645237+Paulo-Eli@users.noreply.github.com>
2022-11-09 23:06:27 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+ function changeImage(){
+    const el = document.querySelector("img");
+    el.setAttribute("src","MotoTecPro1.jpg");
+    const p = document.querySelector("p");
+    p.innerText = "PARABÊNS, GANHOU DESCONTO!!!"
+ }
